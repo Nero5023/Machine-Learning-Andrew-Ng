1b3d3aae8ca090b168a72900204f1cf2981dcbe2
Nero Zuo <502350123@qq.com>
2016-11-30 15:59:17 +0800

    gradientDescent

--- week2/machine-learning-ex1/ex1/gradientDescent.m
+++ week2/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,9 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    
+    temp = (X*theta-y)'*X; % the value behine ∑ (mx2)
+    SUM = sum(temp,1); % 1*2
+    theta = theta - alpha/m*SUM';
 
 
 
